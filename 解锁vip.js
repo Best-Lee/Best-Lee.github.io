9e25166d28d58935c81ed2fcb38b3aa38522b99b
Best-Lee <93002395+Best-Lee@users.noreply.github.com>
2023-12-07 10:35:27 +0800

    Update 解锁vip.js

--- 解锁vip.js
+++ 解锁vip.js
@@ -1,15 +1,17 @@
-re('"userVipInfo"\\s*:\\s*{\\s*"isValid"\\s*:\\s*false,\\s*"originalTransactionId"\\s*:\\s*null,\\s*"level"\\s*:\\s*0,\\s*"productId"\\s*:\\s*null\\s*}','"userVipInfo": {"isValid": true, "originalTransactionId": "510001408663730", "vipExpireTime": 1702516666000, "level": 1, "productId": "vip_yearly_trial", "vipCountStartTime": 1701302073000}');
-
-function re(){
-    var body = $response.body;
-    if(arguments[0].includes("@")){
-        var n = arguments[0].split("@"), r = arguments[1].split("@");
-        for(i = 0; i < n.length; i++){
-            var l = new RegExp(n[i], "g"), body = body.replace(l, r[i]);
-        }
-    } else {
-        var l = new RegExp(arguments[0], "g");
-        body = body.replace(l, arguments[1]);
+let response = {
+    "userVipInfo": {
+        "isValid": false,
+        "originalTransactionId": null,
+        "level": 0,
+        "productId": null
     }
-    $done(body);
-}
+};
+
+response.userVipInfo = {
+    "isValid": true,
+    "originalTransactionId": "510001408663730",
+    "vipExpireTime": 1732924473000,
+    "level": 1,
+    "productId": "vip_yearly_trial",
+    "vipCountStartTime": 1701302073000
+};
