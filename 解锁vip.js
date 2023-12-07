e1c744e263103fb6511df82b5e181ac2d2e9683c
Best-Lee <93002395+Best-Lee@users.noreply.github.com>
2023-12-07 10:45:30 +0800

    Update 解锁vip.js

--- 解锁vip.js
+++ 解锁vip.js
@@ -1,5 +1,5 @@
-let response = {
-    "userVipInfo" : {
+let jsonString = `{
+  "userVipInfo" : {
       "isValid" : true,
       "originalTransactionId" : "510001408663730",
       "vipExpireTime" : 1702511666000,
@@ -7,13 +7,20 @@ let response = {
       "productId" : "vip_monthly_trial",
       "vipCountStartTime" : 1701302073000
     }
-};
+}`;
 
-response.userVipInfo = {
-    "isValid": true,
-    "originalTransactionId": "510001408663730",
-    "vipExpireTime": 1732924473000,
-    "level": 1,
-    "productId": "vip_yearly_trial",
-    "vipCountStartTime": 1701302073000
-};
+// 将字符串解析为 JSON 对象
+let jsonObj = JSON.parse(jsonString);
+
+// 直接修改 JSON 对象
+jsonObj.userVipInfo.isValid = true;
+jsonObj.userVipInfo.originalTransactionId = "510001408663730";
+jsonObj.userVipInfo.vipExpireTime = 1732924473000;
+jsonObj.userVipInfo.level = 1;
+jsonObj.userVipInfo.productId = "vip_yearly_trial";
+jsonObj.userVipInfo.vipCountStartTime = 1701302073000;
+
+// 将修改后的对象转换回字符串
+let modifiedJsonString = JSON.stringify(jsonObj, null, 2); // 第二个和第三个参数用于格式化输出
+
+console.log(modifiedJsonString);
