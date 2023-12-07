71c6d425b85c59ae4a9fcd669f2070fb95f5d92d
Best-Lee <93002395+Best-Lee@users.noreply.github.com>
2023-12-07 10:58:42 +0800

    Update 解锁vip.js

--- 解锁vip.js
+++ 解锁vip.js
@@ -1,24 +1,36 @@
 let jsonString = `{
-  "userVipInfo" : {
-      "isValid" : true,
-      "originalTransactionId" : "510001408663730",
-      "vipExpireTime" : 1702511666000,
-      "level" : 1,
-      "productId" : "vip_monthly_trial",
-      "vipCountStartTime" : 1701302073000
-    }
+  "ret": 0,
+  "msg": "success",
+  "data": {
+    "gender": 0,
+    "uid": "84b9f9d66b3c4adea2cdd292d86251b2",
+    "country": null,
+    "id": "104643340",
+    "nickname": "apple登录用户",
+    "city": null,
+    "headImage": "/user_head_images/default.png",
+    "phoneNumber": null,
+    "platform": 2,
+    "userVipInfo": {
+      "isValid": false,
+      "originalTransactionId": null,
+      "level": 0,
+      "productId": null
+    },
+    "province": null
+  }
 }`;
 
 // 将字符串解析为 JSON 对象
 let jsonObj = JSON.parse(jsonString);
 
-// 直接修改 JSON 对象
-jsonObj.userVipInfo.isValid = true;
-jsonObj.userVipInfo.originalTransactionId = "510001408663730";
-jsonObj.userVipInfo.vipExpireTime = 1732924473000;
-jsonObj.userVipInfo.level = 1;
-jsonObj.userVipInfo.productId = "vip_yearly_trial";
-jsonObj.userVipInfo.vipCountStartTime = 1701302073000;
+// 直接修改 JSON 对象中的 userVipInfo 部分
+jsonObj.data.userVipInfo.isValid = true;
+jsonObj.data.userVipInfo.originalTransactionId = "510001408663730";
+jsonObj.data.userVipInfo.vipExpireTime = 1732924473000;
+jsonObj.data.userVipInfo.level = 1;
+jsonObj.data.userVipInfo.productId = "vip_yearly_trial";
+jsonObj.data.userVipInfo.vipCountStartTime = 1701302073000;
 
 // 将修改后的对象转换回字符串
 let modifiedJsonString = JSON.stringify(jsonObj, null, 2); // 第二个和第三个参数用于格式化输出
